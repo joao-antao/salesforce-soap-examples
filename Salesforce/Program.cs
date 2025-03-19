f62235b2d9f1f8f17e049dea8d1e8d4e8e4de0d5
João A. <joaoantao@funda.nl>
2025-03-19 17:23:27 +0100

    Update Program.cs

--- Salesforce/Program.cs
+++ Salesforce/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Xml.Linq;
+using System.Xml.Serialization;
 using Salesforce;
 
 // DO NOT USE THIS CODE IN ANY PRODUCTION ENVIRONMENT.
@@ -8,27 +9,29 @@ const string password = "YOUR_PASSWORD";
 const string token = "YOUR_TOKEN";
 const string loginUrl = "https://test.salesforce.com/services/Soap/u/63.0";
 
-var loginContent = new LoginHttpContent(username, password, token);
-var loginRequest = new HttpRequestMessage(HttpMethod.Post, loginUrl)
-{
-    Content = loginContent,
-    Headers = { { "SOAPAction", "login" } }
-};
+using var loginContent = new LoginHttpContent(username, password, token);
+using var loginRequest = new HttpRequestMessage(HttpMethod.Post, loginUrl);
+loginRequest.Content = loginContent;
+loginRequest.Headers.Add("SOAPAction", "login");
 
-var loginResponse = await new HttpClient().SendAsync(loginRequest);
-var loginResponseContent = await loginResponse.Content.ReadAsStringAsync();
-var loginResponseXml = XDocument.Parse(loginResponseContent);
-var login = new LoginResponse(loginResponseXml.Descendants(XName.Get("serverUrl", "urn:partner.soap.sforce.com")).FirstOrDefault()?.Value, 
-    loginResponseXml.Descendants(XName.Get("sessionId", "urn:partner.soap.sforce.com")).FirstOrDefault()?.Value);
+using var loginResponse = await new HttpClient().SendAsync(loginRequest);
+var loginResponseXml = XDocument.Parse(await loginResponse.Content.ReadAsStringAsync())
+    .Descendants(XNamespace.Get(urn) + "result").First().ToString();
+
+var serializer = new XmlSerializer(typeof(LoginResponse));
+LoginResponse? login;
+using (var stringReader = new StringReader(loginResponseXml))
+{
+    login = (LoginResponse)serializer.Deserialize(stringReader);
+}
 
 var soqlQuery = "SELECT Id, Name, Email FROM Contact LIMIT 5";
-var queryContent = new QueryHttpContent(login.SessionId, soqlQuery);
-var queryRequest = new HttpRequestMessage(HttpMethod.Post, login.ServerUrl)
-{
-    Content = queryContent,
-    Headers = { { "SOAPAction", "\"\"" } }
-};
-var queryResponse = await new HttpClient().SendAsync(queryRequest);
+using var queryContent = new QueryHttpContent(login.SessionId, soqlQuery);
+using var queryRequest = new HttpRequestMessage(HttpMethod.Post, login.ServerUrl);
+queryRequest.Content = queryContent;
+queryRequest.Headers.Add("SOAPAction", "\"\"");
+
+using var queryResponse = await new HttpClient().SendAsync(queryRequest);
 if (queryResponse.IsSuccessStatusCode)
 {
     // Deserialize your data
