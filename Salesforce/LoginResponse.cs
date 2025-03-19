5e2f751d8655fa03a60717df201585092818f3df
João A. <joaoantao@funda.nl>
2025-03-19 17:23:21 +0100

    Update LoginResponse.cs

--- Salesforce/LoginResponse.cs
+++ Salesforce/LoginResponse.cs
@@ -1,7 +1,25 @@
-﻿namespace Salesforce;
+﻿using System.Xml.Serialization;
 
-public sealed class LoginResponse(string? serverUrl, string? sessionId)
+namespace Salesforce;
+
+[XmlRoot(Namespace = "urn:partner.soap.sforce.com", ElementName = "result", IsNullable = true)]
+public sealed class LoginResponse
 {
-    public string? ServerUrl { get; } = serverUrl;
-    public string? SessionId { get; } = sessionId;
+    [XmlElement(ElementName = "metadataServerUrl")]
+    public string? MetadataServerUrl;
+
+    [XmlElement(ElementName = "passwordExpired")]
+    public bool PasswordExpired;
+
+    [XmlElement(ElementName = "sandbox")]
+    public bool Sandbox;
+
+    [XmlElement(ElementName = "serverUrl")]
+    public string? ServerUrl;
+
+    [XmlElement(ElementName = "sessionId")]
+    public string? SessionId;
+
+    [XmlElement(ElementName = "userId")]
+    public string? UserId;
 }
